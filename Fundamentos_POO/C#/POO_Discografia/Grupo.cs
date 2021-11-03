dfeb1b0d1dc614d902f7f95c4cc9e8b9bc9aae18
Axel Rodolfo Diez Barroso López (Diez) <88562299+UnityFramx@users.noreply.github.com>
2021-11-03 14:15:04 -0600

    Add files via upload

---
+++ Fundamentos_POO/C#/POO_Discografia/Grupo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_Discografia
+{
+    class Grupo
+    {
+        //ATRIBUTOS ← ESTO ES LO QUE TIENE DE CARACTERISTICAS LA CLASE
+        /*EJEMPLO: Coche → Marca, Color, etc...*/
+         private string _disco;
+
+        //CONSTRUCTOR (PD: PARA CREARLO DEN CLICK DERECHO A LA CLASE>>ACCIONES RAPIDAS>>GENERAR CONSTRUCTOR)
+        public Grupo(string disco)
+        {
+            this._disco = disco;
+        }
+        //ENCAPSULAMIENTO DE NOMBRE      
+        public string disco { get => _disco; set => _disco = value; }
+
+        public override string ToString()
+        {
+          return "Album: "+ disco; //REGRESA EL VALOR PARA IMPRIMIRLO 
+        }
+    }
+}
---
+++ Fundamentos_POO/C#/POO_Discografia/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace POO_Discografia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string minombre, nombreg;
+            Console.WriteLine("Ingresa tu nombre:");
+            minombre= Console.ReadLine(); 
+            Console.WriteLine("Ingresa tu grupo favorito:");
+            nombreg= Console.ReadLine();
+            //CREAMOS LOS OBJETOS g
+            Grupo g = new Grupo("Rust in peace");
+            Grupo g1 = new Grupo("Youthanasia");
+            Grupo g2 = new Grupo("Peace Sells");
+            Grupo g3 = new Grupo("Killing is my business");
+            //IMPRIMIMOS LOS RESULTADOS
+            Console.WriteLine(minombre + " aquí está la discografia del grupo " + nombreg);
+            Console.WriteLine("*********DISCOGRAFIA************");
+            Console.WriteLine(g.ToString()); //ESTE ToString() es el de la clase Grupo 
+            Console.WriteLine(g1.ToString());
+            Console.WriteLine(g2.ToString());
+            Console.WriteLine(g3.ToString());
+            Console.WriteLine("Presione Enter...");
+            Console.ReadLine();
+        }
+    }
+}
